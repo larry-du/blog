8de2ad7aafc73730a72cc95f42007a6ea7d22838
larrydu <larrydu@msi.com>
2022-11-16 18:28:43 +0800

    update

--- docs/utils/createRoute.js
+++ docs/utils/createRoute.js
@@ -5,17 +5,31 @@ function createRoute(folderName, navigationTarget) {
   const extension = ".md";
   const excludeFile = "readme.md";
   const absolutePath = path.join(`${__dirname}/../articles`, folderName);
+  // const test = fs.readdirSync(absolutePath);
+  // const item = test.map((current) => {
+  //   const isExtMD = path.extname(current) === extension;
+  //   if (isExtMD) {
+  //     return current;
+  //   } else {
+  //     const subPath = [];
+  //     const subFolder = fs.readdirSync(path.join(absolutePath, current));
+  //     const currentPath = path.join(`${absolutePath}/${current}`, subFolder);
+  //     subPath.push(currentPath);
+  //     return subPath;
+  //   }
+  // });
+  // console.log(item);
 
   const linkItem = fs
     .readdirSync(path.join(absolutePath))
     .filter((fileName) => {
-      const isExcludeFile = fileName.toLowerCase() === excludeFile;
+      const isReadMeFile = fileName.toLowerCase() === excludeFile;
       const isNormalFile = fs
         .statSync(path.join(absolutePath, fileName))
         .isFile();
       const isExtMD = path.extname(fileName) === extension;
 
-      return !isExcludeFile && isNormalFile && isExtMD;
+      return !isReadMeFile && isNormalFile && isExtMD;
     })
     .map((fileName) => {
       const navItemName = fileName.replace(/.md/i, "");
