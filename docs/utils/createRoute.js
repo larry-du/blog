be9c8f159b24a2e86ec01d4a82e8ab8db5829121
larrydu <larrydu@msi.com>
2023-03-02 18:25:42 +0800

    test local merge

--- docs/utils/createRoute.js
+++ docs/utils/createRoute.js
@@ -2,7 +2,7 @@ import path from "path";
 import fs from "fs";
 
 function createRoute(folderName, navigationTarget) {
-  const extension = ".md";
+  const extensiona = ".md";
   const excludeFile = "readme.md";
   const absolutePath = path.join(`${__dirname}/../articles`, folderName);
 
